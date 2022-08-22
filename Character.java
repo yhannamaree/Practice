f2f9301479f16a60d0b05c195d08e37970cb1e51
yhannamaree <111513727+yhannamaree@users.noreply.github.com>
2022-08-22 11:11:21 +0800

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,19 @@
+public class Character {
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+
+    public void sayMyName() {
+        System.out.println("Hello I am " + name);
+    }
+        public void sayMyStrength() {
+        System.out.println("Hello My Strength is " + strength);
+    }
+        public void sayMyAgility() {
+        System.out.println("Hello My Agility is " + agility);
+    }
+        public void sayMyIntelligence() {
+        System.out.println("Hello My intelligence is" + intelligence);
+    }
+}
